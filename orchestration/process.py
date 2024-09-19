acb6fc5a4df5115eb07b17efcd4140e2d4e38c9f
Lucas Hahn <lhahn1@me.com>
2024-09-19 16:20:54 -0400

    Improved error handlign

--- orchestration/process.py
+++ orchestration/process.py
@@ -1053,6 +1053,9 @@ class BPAgent:
                     raise e
 
                 step_output = collected_messages[-1]
+                if type(step_output) is not LLMResponse:
+                    raise ValueError("Query returned a malformed response. Please try again.")
+
                 if step_output.data is not None:
                     yield FlowLog(message=f"[BPAgent][run_proc] Output for {proc_step.description}: {str(step_output.data)}")
                 elif step_output.text:
